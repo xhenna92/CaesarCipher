601b69aaa4e4c9c5d30b45fa671062d3fea4e7f3
Henna <henna.ahmed92@gmail.com>
2015-06-22 13:13:52 -0400

    added checkSameCity method and registerChild method

--- Person/Person/main.m
+++ Person/Person/main.m
@@ -18,6 +18,8 @@
 
 - (void)setPhoneNumber:(NSString *)phoneNumber;
 - (NSString *)phoneNumber;
+- (BOOL)checkSameCity:(Person *) name;
+- (Person*)registerChild;
 
 @end
 
@@ -51,13 +53,60 @@
     return _phoneNumber;
 }
 
+- (BOOL)checkSameCity:(Person *) name{
+    
+    if ([name city] == _city) {
+        return YES;
+    }
+    else{
+        return NO;
+    }
+
+}
+
+- (Person*)registerChild{
+    Person* child = [[Person alloc]init];
+    child.phoneNumber = _phoneNumber;
+    child.city = _city;
+    
+    return child;
+
+}
+
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Person *Henna = [[Person alloc]init];
+        [Henna setName: @"Henna"];
+        [Henna setCity: @"New York"];
+        [Henna setPhoneNumber: @"7187777777"];
+        
+        Person *Bob = [[Person alloc]init];
+        [Bob setName: @"Bob"];
+        [Bob setCity: @"New Jersey"];
+        [Bob setPhoneNumber: @"7187777778"];
+        
+        Person *Clara = [[Person alloc]init];
+        [Clara setName: @"Clara"];
+        [Clara setCity: @"New York"];
+        [Clara setPhoneNumber: @"7187774778"];
+        
+        BOOL hennaAndClara = [Henna checkSameCity:Clara];
+        BOOL claraAndBob = [Clara checkSameCity:Bob];
+        
+        NSLog(@"%d %d", hennaAndClara, claraAndBob);
+        
+        
+        //A Person has recently had a child, whose name is 'Abc'. Write a method called registerChild which takes 0 parameters and returns a new Person * instance represeting the child, which has the same phoneNumber and city as the parent.
+        
+        Person *Abc = [Henna registerChild];
+        [Abc setName:@"Abc"];
+
+        
+        
     }
     return 0;
 }
